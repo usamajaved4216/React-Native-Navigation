e536278ec7ba97214e3b0adfcbe9655aff2d748b
usamajaved4216 <usamajaved4216@gmail.com>
2024-02-26 17:11:31 +0500

    map real time location practice

--- src/views/updates.js
+++ src/views/updates.js
@@ -4,21 +4,27 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import * as Location from 'expo-location';
 
 function Updates({ navigation }) {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    
+
   useEffect(() => {
     (async () => {
-      
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      Location.watchPositionAsync({
+        accuracy: 6,
+        distanceInterval: 1,
+      }, (newLocation) => {
+        setLocation(newLocation);
+        console.log("🚀 ~ newLocation:", newLocation)
+      });
+
     })();
   }, []);
 
@@ -29,44 +35,48 @@ function Updates({ navigation }) {
     text = JSON.stringify(location);
   }
 
-    return (
-        <>
-            <View style={styles.container}>
-                <Button
-                    title="Press me"
-                    onPress={() => navigation.navigate('Calls')}
-                    />
-                <Text>
-                    Pickup
-                </Text>
-                <MapView style={styles.map}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                  }}
-                >
-                  <Marker
-                    coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-                    title={"My Marker Title"}
-                    description={"This is my marker description"}
-                  />
-                </MapView>
-                    </View>
+  return (
+    <>
+        {location && (
+      <View style={styles.container}>
+        <Button
+          title="Press me"
+          onPress={() => navigation.navigate('Calls')}
+        />
+        <Text>
+          Pickup
+        </Text>
+        <MapView style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0001,
+            longitudeDelta: 0.0001,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title={"My Marker Title"}
+            description={"This is my marker description"}
+          />
+        </MapView>
+      </View>
 
-
-        </>
-    );
+      )}
+    </>
+  );
 }
 
 export default Updates;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    map: {
-        width: '100%',
-        height: '100%',
-    },
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
 });
